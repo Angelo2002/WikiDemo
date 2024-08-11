30bf106ed78480f6165ecefc88ebd453dd7566d1
Angelo <angelo200284@gmail.com>
2024-08-11 11:06:15 -0600

    [add] BreedRepository

---
+++ src/main/java/com/example/wikidemo/Repository/BreedRepository.java
@@ -0,0 +1,9 @@
+package com.example.wikidemo.Repository;
+
+import com.example.wikidemo.Entity.Breed;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BreedRepository extends JpaRepository<Breed, Long> {
+}
