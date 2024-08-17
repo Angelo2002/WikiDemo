00993b55c46a989bc95f4ffbba63fa1ecd307c7a
Angelo <angelo200284@gmail.com>
2024-08-17 09:38:07 -0600

    [add] Security

--- src/main/java/com/example/wikidemo/Controller/WikiController.java
+++ src/main/java/com/example/wikidemo/Controller/WikiController.java
@@ -1,12 +1,22 @@
 package com.example.wikidemo.Controller;
 import com.example.wikidemo.Entity.Breed;
+
+import com.example.wikidemo.Security.CustomUserDetailsService;
+import com.example.wikidemo.Security.JwtUtil;
 import com.example.wikidemo.dto.BreedDTO;
 import com.example.wikidemo.service.WikiService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 
 import java.util.Arrays;
@@ -21,6 +31,7 @@ public class WikiController {
 
 
 
+
     @QueryMapping
     public BreedDTO getBreedById(@Argument long id) {
         return wikiService.getBreedById(id);
@@ -28,7 +39,11 @@ public class WikiController {
 
 
     @QueryMapping
+
     public List<BreedDTO> breeds() {
         return wikiService.getAllBreeds();
     }
+
+
+
 }
